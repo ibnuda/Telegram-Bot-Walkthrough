f1ff615af8634828450f4d3469e36a56669bc93a
Ibnu D. Aji <teratower8@gmail.com>
2018-02-04 21:59:29 +0700

    removed tipes.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,28 @@ module Lib
   ( someFunc
   ) where
 
+import           Model
+import           ReadWrite
+
+import           Data.Text
+
+data ChatContent
+  = IncomeOrExpense Text Double
+  | JustOtherParty Text
+  deriving (Show, Eq)
+
+data ChatModel = ChatModel
+  { content :: ChatContent
+  } deriving (Show, Eq)
+
+data Action
+  = Empty
+  | ActHelp
+  | ActAddInc
+  | ActAddExp
+  | ActMessageText Text
+  | ActMessageDouble Double
+  deriving (Show, Eq)
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn "text"
